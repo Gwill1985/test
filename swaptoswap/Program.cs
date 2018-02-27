677fc65d918def4988c9ea24725ec28421914490
Gmannhts <gmannhtsfarm@outlook.com>
2018-02-26 19:21:58 -0600

    Add project files.

---
+++ swaptoswap/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swaptoswap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int  num1, num2, temp;
+
+            Console.WriteLine("Enter first number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number ");
+            num2 = int.Parse(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+     
+            
+
+            Console.WriteLine("Your numbers swapped are : " );
+            Console.WriteLine("your first number is :" + num1);
+            Console.WriteLine("your second number is :" + num2);
+
+
+            
+            
+
+            Console.ReadLine();
+
+           
+            
+        }
+    }
+}
